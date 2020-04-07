962a2cc38be33e592371785f000f06266fdcd007
Tomasz Kot <kottomasz98@gmail.com>
2020-04-07 13:12:26 +0200

    Add root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
+  root to: 'products#index'
   resources :products, only: :index
   post 'products/search', to: 'products#simple_search'
 end
